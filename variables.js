b7a782026c020dd87b77e40ba4cadbfb9767ccd3
james medina <jamesmedina7787@outlook.com>
2018-07-14 19:45:39 -0400

    variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const tableName = 'sqz'
+const dialect = 'postgres'
+const pw = ''
+const port = '5432'
+const localHost = 'localhost'
+
+module.exports = {
+  tableName,
+  dialect,
+  pw,
+  port,
+  localHost
+}
